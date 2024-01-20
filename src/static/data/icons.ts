3e2ad55b7c90c7e06a901afc9f4facdac4a7d376
rediska <sherstnev.denis.v@gmail.com>
2024-01-20 18:02:33 +0300

    icons

--- src/static/data/icons.ts
+++ src/static/data/icons.ts
@@ -2,6 +2,7 @@ import DOCXIcon from '../../static/icons/DOCX.svg';
 import JPGIcon from '../../static/icons/JPG.svg';
 import TXTIcon from '../../static/icons/TXT.svg';
 import XSLIcon from '../../static/icons/XLS.svg';
+import PDFIcon from '../../static/icons/PDF.svg'
 
 interface IFileIcons {
     [index: string]: string,
@@ -13,4 +14,5 @@ export const fileIcons: IFileIcons = {
     'TXT': TXTIcon,
     'XLS': XSLIcon,
     'XLSX': XSLIcon,
+    'PDF': PDFIcon,
 };
